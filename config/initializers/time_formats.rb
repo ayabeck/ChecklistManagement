98d6b9793af783280a3566993b8776ce2e5ff189
ayabeck <aki@AkNMB.local>
2016-07-01 09:47:42 +0900

    Add time zone and time formats

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,3 @@
+Date::DATE_FORMATS[:default]     = "%Y-%m-%d"       # 2013-11-03
+DateTime::DATE_FORMATS[:default] = "%Y-%m-%d"       # 2013-11-03 14:22:18
+Time::DATE_FORMATS[:default]     = "%Y-%m-%d %H:%M" # 2013-11-03 14:22
