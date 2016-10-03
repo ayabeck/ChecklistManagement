47b1cbc0c4d0114d3ebf5712fab5498515062667
ayabeck <aki@AkNMB.local>
2016-10-03 21:59:33 +0900

    Add auto_link to template.description and help_text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # テキストをsimple_formatで整形した上で
+  # テキスト内に記載されたURLやemail_addressesをリンクとして有効にして返す
+  def auto_link_text(text)
+    auto_link(simple_format(h(text)), :all, target: '_blank' ) do |link|
+      truncate(link, length: 30)
+    end
+  end
 end
