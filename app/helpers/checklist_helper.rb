9203cf4ee84c6576f27acc947232a5bf4028e1bd
ayabeck <aki@AkNMB.local>
2016-10-03 19:44:45 +0900

    Modify template view to using option link helper

--- app/helpers/checklist_helper.rb
+++ app/helpers/checklist_helper.rb
@@ -20,4 +20,11 @@ module ChecklistHelper
       HAML
     end
   end
+
+  def option_link_to(form_item)
+    render inline: <<-HAML, type: :haml, locals: { form_item: form_item }
+= link_to '編集', edit_form_item_path(form_item)
+= link_to '削除', form_item, method: :delete, data: { confirm: t('message.delete_confirm') }, class: 'delete-action'
+    HAML
+  end
 end
