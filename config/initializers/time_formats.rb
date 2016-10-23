9c0a9af1247c7525931e242ae37d4e1a7fee8e40
ayabeck <aki@AkNMB.local>
2016-10-23 10:38:03 +0900

    Modify date format

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,5 @@
 Date::DATE_FORMATS[:default]     = "%Y-%m-%d"       # 2013-11-03
 DateTime::DATE_FORMATS[:default] = "%Y-%m-%d"       # 2013-11-03 14:22:18
 Time::DATE_FORMATS[:default]     = "%Y-%m-%d %H:%M" # 2013-11-03 14:22
+Time::DATE_FORMATS[:date]        = "%Y-%m-%d"       # 2013-11-03
+Time::DATE_FORMATS[:time]        = "%m-%d %H:%M"    # 11-03 14:22
